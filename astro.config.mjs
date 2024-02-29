488df1904cba5f42df8b34644e119c5ff8f0731e
Nicolas Seine <nicolasseine.dev@gmail.com>
2024-02-29 01:11:29 +0100

    remove base site from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://n-seine.github.io",
-  base: "frontend-mentor-newsletter",
   integrations: [tailwind()],
 });
